ce5f7dfe583d0b0f1ab233ba276cec1a2a821947
kokoblack <benzimakareem@yahoo.com>
2023-04-08 22:45:14 +0100

    add teckstack

--- src/pages/TechStack.tsx
+++ src/pages/TechStack.tsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import { TbBrandVscode, TbBrandCypress } from "react-icons/tb";
+import {
+  SiCss3,
+  SiJavascript,
+  SiTypescript,
+  SiReact,
+  SiRedux,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiBootstrap,
+  SiSass,
+  SiTailwindcss,
+  SiGit,
+  SiGithub,
+  SiHtml5,
+} from "react-icons/si";
+import {
+  TSContainer,
+  TSHeader,
+  TSIcon,
+  TSIconContainer,
+  TSText,
+} from "../styles/TechStack";
+import { motion } from "framer-motion";
 
 const TechStack = () => {
-  return (
-    <div>TechStack</div>
-  )
-}
+  const icons = [
+    { icon: <SiHtml5 />, color: "#F1662A", delay: 0.6 },
+    { icon: <SiCss3 />, color: "#33A9DC", delay: 0.7 },
+    { icon: <SiJavascript />, color: "#F5DE19", delay: 0.8 },
+    { icon: <SiTypescript />, color: "#0065A9", delay: 0.9 },
+    { icon: <SiReact />, color: "#00D8FF", delay: 1 },
+    { icon: <SiRedux />, color: "#764ABC", delay: 1.1 },
+    { icon: <SiNextdotjs />, color: "inherit", delay: 1.2 },
+    { icon: <SiNodedotjs />, color: "#68a063", delay: 1.3 },
+    { icon: <SiBootstrap />, color: "#563D7C", delay: 1.4 },
+    { icon: <SiTailwindcss />, color: "#44A8B3", delay: 1.5 },
+    { icon: <SiSass />, color: "#CD6799", delay: 1.6 },
+    { icon: <SiGit />, color: "#DE4C36", delay: 1.7 },
+    { icon: <SiGithub />, color: "inherit", delay: 1.8 },
+    { icon: <TbBrandVscode />, color: "#007ACC", delay: 1.9 },
+    { icon: <TbBrandCypress />, color: "inherit", delay: 2.0 },
+  ];
 
-export default TechStack
+  return (
+    <motion.div
+      initial={{ y: 300 }}
+      whileInView={{ y: 0 }}
+      transition={{ duration: 1 }}
+      viewport={{once: true}}
+    >
+      <TSContainer>
+        <TSHeader>My Tech Stack</TSHeader>
+        <TSText> Technologies I’ve been working with recently</TSText>
+        <TSIconContainer>
+          {icons.map((icon, id) => (
+            <motion.div
+              initial={{ y: 100, opacity: 0 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.5, delay: icon.delay }}
+              viewport={{once: true}}
+            >
+              <TSIcon key={id} color={icon.color}>
+                {icon.icon}
+              </TSIcon>
+            </motion.div>
+          ))}
+        </TSIconContainer>
+      </TSContainer>
+    </motion.div>
+  );
+};
+
+export default TechStack;
--- src/styles/Hero.ts
+++ src/styles/Hero.ts
@@ -7,20 +7,21 @@ export const HeroContainer = styled.div`
   width: 100%;
   padding: 0 4%;
   box-sizing: border-box;
-  margin-top: 8%;
+  /* margin-top: 8%; */
+  height: 100vh;
 
   @media screen and (max-width: 992px) {
-  flex-direction: column;
-}
+    flex-direction: column;
+  }
 `;
 
-export const HeroBasis = styled.div<{basis: number}>`
-width: ${({basis}) => basis}%;
+export const HeroBasis = styled.div<{ basis: number }>`
+  width: ${({ basis }) => basis}%;
 
-@media screen and (max-width: 992px) {
-  width: 100%;
-}
-`
+  @media screen and (max-width: 992px) {
+    width: 100%;
+  }
+`;
 
 export const HeroText = styled.p`
   font-weight: 700;
@@ -29,8 +30,8 @@ export const HeroText = styled.p`
   text-align: left;
 
   @media screen and (max-width: 992px) {
-  text-align: center;
-}
+    text-align: center;
+  }
 `;
 
 export const HeroTextSpan = styled.div`
@@ -39,32 +40,25 @@ export const HeroTextSpan = styled.div`
   -webkit-text-fill-color: transparent;
 `;
 
-export const HeroImgContainer = styled.div`
-padding: .2rem .5rem;
-border-radius: 100%;
-background: linear-gradient(360deg, #13b0f5 -2.06%, #e70faa 100%);
-`
-
 export const HeroImg = styled.img`
-width: 60%;
-display: block;
-margin-left: auto;
-border-radius: 100%;
-padding: .5%;
-background: linear-gradient(360deg, #13b0f5 -2.06%, #e70faa 100%);
+  width: 60%;
+  display: block;
+  margin-left: auto;
+  border-radius: 100%;
+  padding: 0.5%;
+  background: linear-gradient(360deg, #13b0f5 -2.06%, #e70faa 100%);
 
-@media screen and (max-width: 992px) {
-  margin: 0 auto;
-  width: 30%;
+  @media screen and (max-width: 992px) {
+    margin: 0 auto;
+    width: 30%;
+  }
 
-}
-
-@media screen and (max-width: 480px) {
-  width: 45%;
-}
-`
+  @media screen and (max-width: 479px) {
+    width: 45%;
+  }
+`;
 export const HeroBR = styled.br`
-@media screen and (max-width: 992px) {
-  display: none;
-}
-`
+  @media screen and (max-width: 992px) {
+    display: none;
+  }
+`;
