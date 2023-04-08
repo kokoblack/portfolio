49aa6a023dd366b4d538927e5051032ba0ba326e
kokoblack <benzimakareem@yahoo.com>
2023-04-08 18:31:20 +0100

    add hero section

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import {
+  HeroBasis,
+  HeroBR,
+  HeroContainer,
+  HeroImg,
+  HeroImgContainer,
+  HeroText,
+  HeroTextSpan,
+} from "../styles/Hero";
+import profile from "../assets/prof.jpg";
+import Typewriter from "typewriter-effect";
 
 const Hero = () => {
   return (
-    <div style={{height: "100vh"}}>Hero</div>
-  )
-}
+    <HeroContainer>
+      <HeroBasis basis={60}>
+        <HeroText>
+          Hi &#128075;, <HeroBR />
+          My name is <br />
+          <HeroTextSpan>
+            <Typewriter
+            options={{loop: true}}
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Haruna Kareem.")
+                  .pauseFor(1000)
+                  .deleteAll()
+                  .typeString("I'm a Frontend Developer.")
+                  .start()
+                  
+              }}
+            />
+          </HeroTextSpan>
+        </HeroText>
+      </HeroBasis>
 
-export default Hero
+      <HeroBasis basis={40}>
+        <HeroImg src={profile} />
+      </HeroBasis>
+    </HeroContainer>
+  );
+};
+
+export default Hero;
