a875e24b15cb64534b6b3f607be7fb454fcec1a8
kokoblack <benzimakareem26@gmail.com>
2023-08-05 21:14:20 +0100

    update cv

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -24,8 +24,8 @@ import Logo from "../components/Logo";
 const NavBar = () => {
   const link: { link: string; href: string; delay: number }[] = [
     { link: "Home", href: "#Home", delay: 0.1 },
-    { link: "Tech Stack", href: "#TechStack", delay: 0.3 },
-    { link: "Projects", href: "#Projects", delay: 0.4 },
+    { link: "Projects", href: "#Projects", delay: 0.3 },
+    { link: "Tech Stacks", href: "#TechStack", delay: 0.4 },
     { link: "About", href: "#About", delay: 0.2 },
     { link: "Contacts", href: "#Contacts", delay: 0.5 },
   ];
